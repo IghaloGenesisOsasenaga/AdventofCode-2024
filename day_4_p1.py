8800bd11d81158cd5b6779bf939934079fcf2bef
Ighalo Genesis Osasenaga <ighalogenesis2007@gmail.com>
2024-12-06 15:27:58 +0100

    Fixed day 4 part 1

--- day_4_p1.py
+++ day_4_p1.py
@@ -7,7 +7,7 @@ while curr:
 
 n = len(A[0])
 S = ""
-for x in range(n):
+for i in range(n):
     tmp1, tmp2, tmp3, tmp4, tmp5, = '','','','',''
     for j in range(n):
         tmp1 += A[j][i]
@@ -19,13 +19,11 @@ for x in range(n):
             tmp3 += A[n-k-1][j]
             tmp4 += A[k][j]
         k += 1
-    S += A[i] + '.' + A[i][::-1] + '.' # horizontal 
-    S += tmp1 + '.' +tmp1[::-1] + '.' # vertical
-    S += tmp2 + '.' +tmp2[::-1] + '.' # upper right diagonal (⁰/)
-    S += tmp3 + '.' +tmp3[::-1] + '.' # lower right diagonal (/₀)
-    S += tmp4 + '.' +tmp4[::-1] + '.' # upper left diagonal (\⁰)
-    S += tmp5 + '.' +tmp5[::-1] + '.' # lower left diagonal (₀\)
-# m s  s m  m m  s s
-#  a    a    a    a
-# m s  s m  s s  m m
+    S += A[i]+ '.' +A[i][::-1]+ '.' # horizontal 
+    S += tmp1+ '.' +tmp1[::-1]+ '.' # vertical
+    S += tmp2+ '.' +tmp2[::-1]+ '.' # upper right diagonal (⁰/)
+    S += tmp3+ '.' +tmp3[::-1]+ '.' # lower right diagonal (/₀)
+    S += tmp4+ '.' +tmp4[::-1]+ '.' # upper left diagonal (\⁰)
+    S += tmp5+ '.' +tmp5[::-1]+ '.' # lower left diagonal (₀\)
+
 print(S.count("XMAS"))
